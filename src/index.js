0c46b41a88b96479ca647df4cbef2dec7d58bc23
Roncea Catalin <catalin.roncea@gmail.com>
2021-04-04 13:02:18 +0300

    add roboto fonts

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './fonts/Roboto/Roboto-Bold.ttf';
+import './fonts/Roboto/Roboto-BoldItalic.ttf';
+import './fonts/Roboto/Roboto-Regular.ttf';
+import './fonts/Roboto/Roboto-Light.ttf';
+import './fonts/Roboto/Roboto-LightItalic.ttf';
+import './fonts/Roboto/Roboto-Medium.ttf';
+import './fonts/Roboto/Roboto-MediumItalic.ttf';
+import './fonts/Roboto/Roboto-Thin.ttf';
+import './fonts/Roboto/Roboto-ThinItalic.ttf';
+import './fonts/Roboto/Roboto-Black.ttf';
+import './fonts/Roboto/Roboto-BlackItalic.ttf';
 
 ReactDOM.render(
   <React.StrictMode>
